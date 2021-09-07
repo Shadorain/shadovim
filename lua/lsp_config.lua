a4ff1f562584b8bd6abf973e78ae0c22baec54bb
Shadorain <shadow@Shadowraith.fios-router.home>
2021-09-07 12:58:44 -0400

    LSP error sign change back

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -48,7 +48,7 @@ end
 -- }}}
 -- Sign defining {{{
 -- local signs = { Error = "× ", Warning = " ", Hint = " ", Information = " " }
-local signs = { Error = "⌦ ", Warning = "🞜 ", Hint = "", Information = "🞧 " }
+local signs = { Error = "⮀ ", Warning = "🞜 ", Hint = "", Information = "🞧 " }
 for type, icon in pairs(signs) do
   local hl = "LspDiagnosticsSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
