b8ffffae08d5593b6385d7ee66a58fda706a8162
Shadorain <jordanf7517@gmail.com>
2022-07-18 18:47:00 -0400

    case sensitive hopping

--- lua/config.lua
+++ lua/config.lua
@@ -288,7 +288,9 @@ local status_ok, hop = pcall(require, "hop")
 if not status_ok then
 	return
 end
-hop.setup()
+hop.setup {
+  case_insensitive = false,
+}
 --- }}}
 --- Winbar {{{
 winbar_filetype_exclude = {
