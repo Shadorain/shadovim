3430c6ad8c7a6a431a7ef73e96f6d5af511bc0f9
Shadorain <jordanf7517@gmail.com>
2024-04-01 00:30:51 -0400

    feat: Added obsidian.nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -290,6 +290,24 @@ return {
 			notifiers = { { name = "Default", opts = { sticky = false } } },
 		},
 	},
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		event = {
+			"BufReadPre " .. vim.fn.expand("~") .. "/Documents/Sync/ObsidianVault/**.md",
+			"BufNewFile " .. vim.fn.expand("~") .. "/Documents/Sync/ObsidianVault/**.md",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"epwalsh/pomo.nvim",
+		},
+		opts = {
+			workspaces = { { name = "personal", path = "~/Documents/Sync/ObsidianVault/" } },
+		},
+	},
 
 	-- Theme
 	{
